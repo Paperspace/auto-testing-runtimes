615fff840e0786cad98f419d241b2cc139e2817d
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2022-08-18 21:59:29 -0700

    Add files via upload

--- run_testing_pt112.sh
+++ run_testing_pt112.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-jupyter nbconvert --to notebook --execute quick_start_pytorch.ipynb --allow-errors --output-dir results
+jupyter nbconvert --to notebook --execute /notebooks/quick_start_pytorch.ipynb --allow-errors --output-dir /notebooks/auto_testing_results
 
 echo Testing is done
