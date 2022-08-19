7b60c1ba846bb52fb1e515f97e6049c8fded9cda
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2022-08-18 21:17:15 -0700

    Drafting testing scripts

--- do_run_testing.sh
+++ do_run_testing.sh
@@ -1,90 +1,155 @@
 #!/bin/bash
 #
-# Requirements:
+# See repo for how to use this script:
+# https://github.com/Paperspace/test-updated-runtimes
 #
-# Gradient CLI
-# User subscription tier that allows many Notebooks
+# Last updated: Aug 18th 2022
+
+
+# -----------------------------------------
+# Set to ID of own created Gradient Project
+
+#projectId="<Project ID>"
+projectId="p3lkwjx67pw"
+# ------------------------------------------
+
+
+# Loop through each valid runtime+machine combination
 #
-# Last updated: Aug 10th 2022
+# for given runtime
+#   foreach machine
+#     create Notebook
+#     run tests
+#
+# For R runtimes and M machines, this would creates R*M Gradient Notebooks
+# So script is called for one runtime at once
+#
+# Ideally the machines list would be the same for all our runtimes, but some combinations are incompatible
+# This could be expressed as a support matrix, but currently we simply loop on the compatible combinations within each runtime
+# The current machines list follows the Gradient documentation at https://docs.paperspace.com/gradient/machines/
+# More information on our runtimes is at https://www.notion.so/paperspace/Tables-of-current-runtimes-7d2580b067dc45919b3220bc56ec9eda
+# Runtimes are divided into ours, which use our base Docker image (or another one beginning with "paperspace") + a repo, and partners', which use their Docker images
+# Our base runtime is tested directly via the "Start from Scratch" runtime below, which is the base with no repo
+# Notebooks CLI reference is at https://docs.paperspace.com/gradient/cli/notebooks
 
-projectId="<Project ID>" # Set to own project
+# Current machines
 
+# CPU: C5, C7
+# GPU: P4000, RTX4000, RTX5000, P5000, P6000, A4000, V100, V100-32G, A5000, A6000, A100, A100-80G
+# Multi-GPU: A4000x2, V100-32Gx2, V100-32Gx4, A5000x2, A6000x2, A6000x4, A100x2
+# Free: Free CPU (C4), Free GPU+ (M4000), Free P4000, Free RTX4000, Free P5000, Free RTX5000, Free A4000, Free A5000, Free A6000, Free A100-80G
+# IPU: Free-IPU-POD16
 
-# Our runtimes
-# ------------
+# Current runtimes
 
-# Start same Notebook for different machines
-# Ideally machines list same for all our runtimes, and only partner ones have any incompatible machines, but may not hold
+# Ours: PyTorch 1.12, TensorFlow 2.9.1, Paperspace + Fast.AI, DALL-E Mini, Transformers+NLP, Start from Scratch, ClipIt-PixelDraw
+# Partner: NVIDIA RAPIDS, Hugging Face Optimum on IPU, PyTorch on IPU, TensorFlow 2 on IPU
 
-machines=("P4000" "P5000") # TODO - add rest
+# Total combinations <= 32*11 = 354
+
+# Global settings
+
+runtime=$1
 base="paperspace/gradient-base:pt112-tf29-jax0314-py39-20220803"
-#command='...' # Doesn't parse below correctly
+
 
 # PyTorch 1.12
+# ------------
 
-runtime="PyTorch 1.12"
-workspace="https://github.com/gradient-ai/PyTorch"
-#name="..." # Doesn't parse below correctly
+if [ "$runtime" = "PyTorch 1.12" ]; then
+    
+  machines=("P4000")
+  #machines=("P4000" "P5000")
+  #machines=("P4000", "RTX4000", "RTX5000", "P5000", "P6000", "A4000", "V100", "V100-32G", "A5000", "A6000", "A100", "A100-80G")
 
-for machine in ${machines[@]}; do
+  workspace="https://github.com/gradient-ai/PyTorch"
+  #name="..." # Doesn't parse correctly if placed here, so currently directly in the CLI invocation
+  #command='...' # Doesn't parse correctly if placed here, so currently directly in the CLI invocation
+  #There might be a better redirect than 2>&1 | tee, but we want to capture stdout and stderr
+  #If gradient notebooks create is same for every runtime can bring out into function
+  tag="autotestingpytorch112" # Tags can only contain alphanumeric characters
+
+  echo Runtime $runtime
+
+  for machine in ${machines[@]}; do
     
     echo Machine $machine
 
     gradient notebooks create \
-	--machineType $machine \
-        --container $base \
-        --projectId $projectId \
-        --name "Runtime = $runtime, Machine = $machine" \
-        --command 'git clone https://github.com/Paperspace/test-updated-runtimes && cd test-updated-runtimes && chmod 764 run_testing.sh && ./run_testing.sh 2>&1 | tee run_testing.log & echo "And now JupyterLab" && jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin="*" --ServerApp.allow_credentials=True' \
-        --workspace $workspace
-    
-done
+      --machineType $machine \
+      --container $base \
+      --projectId $projectId \
+      --name "Auto-testing: $runtime on $machine" \
+      --command 'git clone https://github.com/Paperspace/test-updated-runtimes && cd test-updated-runtimes && chmod 764 run_testing.sh && ./run_testing.sh $runtime 2>&1 | tee run_testing.log & echo "And now JupyterLab" && PIP_DISABLE_PIP_VERSION_CHECK=1 && jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin="*" --ServerApp.allow_credentials=True' \
+      --workspace $workspace \
+      --tag $tag
 
-# TODO
+  done
+
+fi
+
 
 # TensorFlow 2.9.1
-# Paperspace+Fast.ai
-# DALL-E-Mini
-
-# Not doing
-
-# Transformers + NLP
-# ClipIt-PixelDraw
-
-
-# Partner runtimes
 # ----------------
 
-# Similar but own containers not base
+if [ "$runtime" = "TensorFlow 2.9.1" ]; then
+    
+  machines=("P4000" "P5000")
+  #machines=("P4000", "RTX4000", "RTX5000", "P5000", "P6000", "A4000", "V100", "V100-32G", "A5000", "A6000", "A100", "A100-80G")
+
+  workspace="https://github.com/gradient-ai/TensorFlow"
+  tag="autotestingtensorflow291"
+
+  echo Runtime $runtime
+
+  for machine in ${machines[@]}; do
+    
+    echo Machine $machine
+
+    gradient notebooks create \
+      --machineType $machine \
+      --container $base \
+      --projectId $projectId \
+      --name "Auto-testing: $runtime on $machine" \
+      --command 'git clone https://github.com/Paperspace/test-updated-runtimes && cd test-updated-runtimes && chmod 764 run_testing.sh && ./run_testing.sh $runtime 2>&1 | tee run_testing.log & echo "And now JupyterLab" && PIP_DISABLE_PIP_VERSION_CHECK=1 && jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True --ServerApp.allow_origin="*" --ServerApp.allow_credentials=True' \
+      --workspace $workspace \
+      --tag $tag
+
+  done
+
+fi
 
 # TODO
 
-# NVIDIA RAPIDS
-# Graphcore IPU
+#Paperspace + Fast.AI
+#DALL-E Mini
+#Transformers+NLP
+#Start from Scratch
+#ClipIt-PixelDraw
+#NVIDIA RAPIDS
+#Hugging Face Optimum on IPU
+#PyTorch on IPU
+#TensorFlow 2 on IPU
 
 
-# Base image / Gradient runtime
-# -----------------------------
+# Improvements
+# ************
 
-# Also similar, no workspace
+# Better would be to create one Notebook for each runtime and run it for all machines
+# This would avoid creating a large number of Notebooks
+# But using gradient notebooks create, stop, and start same ID with different machine still creates a new one each time, leaving just as many
 
-# TODO
+# Potentially add these (the Notebook ID can be retrieved)
 
-
-# Close notebooks after running
-# -----------------------------
-
-# TODO
-
-#gradient notebooks logs \
+#gradient notebooks stop \   # Avoid leaving them all running if can wait for testing in each one to be done before stopping
 #  --id "<Notebook ID>"
 
-#gradient notebooks stop \
+#gradient notebooks logs \   # Retrieve the logs from each Notebook
 #  --id "<Notebook ID>"
   
-#gradient notebooks delete \
+#gradient notebooks delete \   # If results are uploaded elsewhere, don't need to keep each Notebook
 #  --id "<Notebook ID>"
 
-#to add: gradient notebooks metrics get \
+#gradient notebooks metrics get \   # Record resource usage, e.g., GPUs
 #  --id "<Notebook ID>" \
 #  --metric <GPU>, etc.
--- run_testing.sh
+++ run_testing.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 #
-# Run Notebook tests as on "How to test each runtime" Notion page
+# This is part of the repo https://github.com/Paperspace/test-updated-runtimes
 #
-# Last updated: Aug 10th 2022
-
-# References
+# Called by: do_run_testing.sh
+# Calls: ...
 #
-# https://nbconvert.readthedocs.io/en/latest/usage.html
-# https://nbconvert.readthedocs.io/en/latest/install.html
+# Run tests on Notebook for its runtime+machine combination passed in by do_run_testing.sh
+#
+# - CLI
+# - .ipynb notebook content
+#
+# Tests should correspond to the runtimes project page at https://www.notion.so/paperspace/How-to-test-each-runtime-QA-f460aa4513554ae9b5d81ef513044fdf
+#
+# Last updated: Aug 18th 2022
 
 echo "Testing has started"
+
+# Global settings
+
+runtime=$1
+resultsdir="auto_testing_results"
+
+cd /notebooks
+mkdir $resultsdir # Assumes there isn't a directory with this name already
 
 
 # CLI tests
@@ -33,13 +46,17 @@ which python3
 python -c 'import sys; print(sys.version); print(sys.executable)'
 python3 -c 'import sys; print(sys.version); print(sys.executable)'
 
-# Jupyter cell with
+# Jupyter
+
+# TODO: Call notebook with tests, e.g., print(sys.version) checks it's using same Python as shell
 
 #import sys
 #print(sys.version)
 #print(sys.executable)
 
 # Python ecosystem
+
+# TODO: Call .py with Python modules, etc.
 
 #import numpy
 #import scipy
@@ -67,39 +84,46 @@ python3 -c 'import sys; print(sys.version); print(sys.executable)'
 #import datasets 
 #import cv2 # opencv-python
 
-# Gradient commands
+# Gradient CLI
 
-gradient
+which gradient
 gradient version
 
+# ... TODO: Add rest of tests
 
 
-# ... TODO
+# Run Jupyter .ipynb notebooks
+# ----------------------------
 
+# Each runtime has own content, so use if/then
 
-# Run Jupyter notebook
-# --------------------
-
-cd /notebooks
-mkdir results ### Need unique outdir if same Notebook and same .ipynb but different machines
-
-# This runs the notebook in the notebook
+# The nbconvert form given runs the notebook in the notebook
 # Default output is <name>.nbconvert.ipynb
-jupyter nbconvert --to notebook --execute quick_start_pytorch.ipynb --allow-errors --output-dir results
+# https://nbconvert.readthedocs.io/en/latest/usage.html
+# https://nbconvert.readthedocs.io/en/latest/install.html
 
-# PDF output also works and can be saved to a remote location without the need to access the Notebook to inspect the results
-# But it requires a slow TeXLive install
+if [ "$runtime" = "PyTorch 1.12" ]; then
+
+  echo Running notebooks for $runtime
+  jupyter nbconvert --to notebook --execute quick_start_pytorch.ipynb --allow-errors --output-dir results
+
+fi
+
+echo "Testing is done"
+
+
+# Improvements
+# ************
+
+# Include notebook rerun on test and rerun+kernel restart
+
+# PDF output also works and could be saved to a remote location without the need to access the Notebook to inspect the results
+# But it requires a slow TeXLive install, and our Notebooks can be viewed offline
 
 #apt-get update
 #apt-get install -y pandoc
 #apt-get install -y texlive-xetex texlive-fonts-recommended texlive-plain-generic
 #jupyter nbconvert --to pdf --execute quick_start_pytorch.ipynb --allow-errors --output-dir pdf
 
-# [jupytext converts it to script then can see output]
-# [papermill execute notebook with params to notebook]
-
-echo "Testing is done"
-
-# TODO
-
-# Include notebook rerun on test and rerun+kernel restart
+# jupytext converts it to script then can see output
+# Papermill has execute notebook with params to notebook
