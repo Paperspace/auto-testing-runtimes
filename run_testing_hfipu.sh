90a5c524f15d81e7bade177f88d2ce28b9f1f7b3
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2022-08-22 15:46:44 -0700

    Add files via upload

---
+++ run_testing_hfipu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+jupyter nbconvert --to notebook --execute /notebooks/get-started/walkthrough.ipynb                              --allow-errors --output-dir /notebooks/auto_testing_results
+jupyter nbconvert --to notebook --execute /notebooks/notebook-tutorials/introduction_to_optimum_graphcore.ipynb --allow-errors --output-dir /notebooks/auto_testing_results
+jupyter nbconvert --to notebook --execute /notebooks/notebook-tutorials/text_classification.ipynb               --allow-errors --output-dir /notebooks/auto_testing_results
+
+echo Testing is done
