e2ec28471548bb23741cf87d80cc2670a86ddd48
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2022-08-22 15:27:53 -0700

    Add files via upload

--- run_testing_rapids.sh
+++ run_testing_rapids.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#jupyter nbconvert --to notebook --execute /notebooks/ ... .ipynb --allow-errors --output-dir /notebooks/auto_testing_results
-
 # This is only a subset of all the .ipynbs in the RAPIDS repo
 
-jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/arima_demo.ipynb
-jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/forest_inference_demo.ipynb
-jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/kmeans_demo.ipynb
-jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/linear_regression_demo.ipynb
-jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/nearest_neighbors_demo.ipynb
-jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/random_forest_demo.ipynb
+jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/arima_demo.ipynb             --allow-errors --output-dir /notebooks/auto_testing_results
+jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/forest_inference_demo.ipynb  --allow-errors --output-dir /notebooks/auto_testing_results
+jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/kmeans_demo.ipynb            --allow-errors --output-dir /notebooks/auto_testing_results
+jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/linear_regression_demo.ipynb --allow-errors --output-dir /notebooks/auto_testing_results
+jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/nearest_neighbors_demo.ipynb --allow-errors --output-dir /notebooks/auto_testing_results
+jupyter nbconvert --to notebook --execute /notebooks/rapids/cuml/random_forest_demo.ipynb     --allow-errors --output-dir /notebooks/auto_testing_results
 
-jupyter nbconvert --to notebook --execute /notebooks/rapids/xgboost/XGBoost_Demo.ipynb
+jupyter nbconvert --to notebook --execute /notebooks/rapids/xgboost/XGBoost_Demo.ipynb        --allow-errors --output-dir /notebooks/auto_testing_results
 
 echo Testing is done
