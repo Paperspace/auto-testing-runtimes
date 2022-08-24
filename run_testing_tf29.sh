ca1f7fb0c47d0438aee721cc1a6743c84ee32bd7
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2022-08-23 17:56:43 -0700

    Delete run_testing_tf29.sh

--- run_testing_tf29.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-jupyter nbconvert --to notebook --execute /notebooks/quick_start_beginner.ipynb --allow-errors --output-dir /notebooks/auto_testing_results
-jupyter nbconvert --to notebook --execute /notebooks/quick_start_advanced.ipynb --allow-errors --output-dir /notebooks/auto_testing_results
-
-echo Testing is done
