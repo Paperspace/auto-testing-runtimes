b4a82d86634878e35832993bb86dde7708d8b87e
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2022-08-23 17:55:59 -0700

    Delete run_testing_dalle.sh

--- run_testing_dalle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# May fail because this doesn't mount the Gradient Dataset as the notebook says is required
-
-jupyter nbconvert --to notebook --execute /notebooks/DALL-E-Mini-inference-pipeline.ipynb --allow-errors --output-dir /notebooks/auto_testing_results
-
-echo Testing is done
