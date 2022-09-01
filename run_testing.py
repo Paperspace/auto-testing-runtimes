91c690ea5d24bc1038faaa74ecf1bd7c0a8ef2d9
Nick Ball <74800360+nmb-paperspace@users.noreply.github.com>
2022-09-01 11:48:26 -0700

    Add logging

--- run_testing.py
+++ run_testing.py
@@ -1,35 +1,94 @@
 # Generic tests for Python
 #
 # Called by run_testing.sh
+#
+# Last updated: Sep 01st 2022
 
-# Imports should work without error
-import numpy
-import scipy
-import pandas
-import cloudpickle
-import skimage # scikit-image
-import sklearn # scikit-learn
-import matplotlib
-import ipykernel
-import ipywidgets
-import gradient
-import cython #Cython
-import tqdm
-import gdown
-import xgboost 
-import PIL #pillow
-import seaborn
-import sqlalchemy # SQLALchemy
-import spacy
-import nltk
-import jsonify
-import boto3
-import transformers
-import sentence_transformers # sentence-transformers
-import datasets 
-import cv2 # opencv-python
+print('Running generic tests for Python ...' + '\n')
 
 # Should show expected versions
+
+print('Python is expected versions ...' + '\n')
+
 import sys
-print(sys.version)
-print(sys.executable)
+print('sys.version:', sys.version)
+print('sys.executable:', sys.executable)
+
+# Imports should work without error
+
+print('\n' + 'Checking module imports ...' + '\n')
+
+print('numpy')
+import numpy
+
+print('scipy')
+import scipy
+
+print('pandas')
+import pandas
+
+print('cloudpickle')
+import cloudpickle
+
+print('skimage')
+import skimage # scikit-image
+
+print('sklearn')
+import sklearn # scikit-learn
+
+print('matplotlib')
+import matplotlib
+
+print('ipykernel')
+import ipykernel
+
+print('ipywidgets')
+import ipywidgets
+
+print('cython')
+import cython #Cython
+
+print('tqdm')
+import tqdm
+
+print('gdown')
+import gdown
+
+print('xgboost')
+import xgboost 
+
+print('PIL')
+import PIL #pillow
+
+print('seaborn')
+import seaborn
+
+print('sqlalchemy')
+import sqlalchemy # SQLALchemy
+
+print('spacy')
+import spacy
+
+print('nltk')
+import nltk
+
+print('jsonify')
+import jsonify
+
+print('boto3')
+import boto3
+
+print('transformers')
+import transformers
+
+print('sentence_transformers')
+import sentence_transformers # sentence-transformers
+
+print('datasets')
+import datasets 
+
+print('cv2')
+import cv2 # opencv-python
+
+print('gradient')
+import gradient # Gradient will fail on partner containers that don't have it
